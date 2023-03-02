91b1ae245ceddf7f19544fe9a7dc942e276d4fc2
Ben Shutt <ben@3sidedcube.com>
2023-03-02 15:13:31 +0000

    Make base files

--- Sources/HybridKit.swift
+++
@@ -1,6 +0,0 @@
-public struct HybridKit {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/NavigationControllerKey.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationControllerKey.swift
+//  HybridKit
+//
+//  Created by Ben Shutt on 02/03/2023.
+//  Copyright © 2023 3 SIDED CUBE APP PRODUCTIONS LTD. All rights reserved.
+//
+
+import Foundation
